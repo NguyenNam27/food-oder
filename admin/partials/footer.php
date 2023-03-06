9340dc1d6f59471328f4e4641072cf2e19200e91
NguyenNam27 <nguyenngocnam00770@gmail.com>
2023-03-07 00:05:40 +0700

    update

---
+++ admin/partials/footer.php
@@ -0,0 +1,7 @@
+<!-- Footer Section starts -->
+<div class="footer">
+    <div class="wrapper">
+        <p class="text-center">2022 All rights reserved,Food House.Doveloped By - <a href="">Nam</a></p>
+    </div>
+</div>
+<!-- Footer Section ends -->
---
+++ admin/partials/menu.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Food Order Website - Home Page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<!-- Menu Section starts -->
+<div class="menu text-center">
+    <div class="wrapper">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="manage-admin.php">Admin Manager</a></li>
+            <li><a href="manage-category.php">Category</a></li>
+            <li><a href="manage-food.php">Food</a></li>
+            <li><a href="manage-order.php">Order</a></li>
+
+        </ul>
+
+    </div>
+
+</div>
+<!-- Menu Section ends -->
